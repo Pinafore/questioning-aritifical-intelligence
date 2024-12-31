056ee66e66483bd0c86f1a2512503d33f747a847
Jordan Ying <jbg@Mini-von-Jordan.home.arpa>
2024-12-31 17:50:41 -0500

    improve blurbs

--- scripts/split_blurbs.py
+++ scripts/split_blurbs.py
@@ -17,6 +17,16 @@ if __name__ == "__main__":
                 
             outfile.write(text)
 
+            sections = []
             for ii in subsections:
-                label, section_name = ii
-                outfile.write("\\invisiblesection{%s}{%s}\n" % (section_name, label))
+                if len(ii) > 2:
+                    label, section_name, description = ii
+                    sections.append((section_name, description))
+                else:
+                    label, section_name = ii
+                outfile.write("\n\\invisiblesection{%s}{%s}\n\n" % (section_name, label))
+
+            if len(sections) > 1:
+                outfile.write("Topics in chapter:\\n\\begin{enumerate*}")
+                outfile.write("\n\t".join("\\item \\textbf{%s:} %s" % (x, y) for x, y in sections))
+                outfile.write("\n\\end{enumerate*}")
